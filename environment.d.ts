79943caec982651750e40f5a2f01a2a90fcc501d
Afonso Escudeiro <afonso_escudeiro@hotmail.com>
2022-01-31 22:33:32 -0100

    create signature route

--- environment.d.ts
+++ environment.d.ts
@@ -4,6 +4,7 @@ declare global {
             HASURA_URI: string;
             NODE_ENV: "development" | "production";
             PORT?: string;
+            JWT_SECRET: string;
         }
     }
 }
--- routes/auth/auth.ts
+++ routes/auth/auth.ts
@@ -6,7 +6,16 @@ import {
     HttpLink,
 } from "@apollo/client/core";
 import fetch from "cross-fetch";
-import { isValidChecksumAddress } from "ethereumjs-util";
+import jwt from "jsonwebtoken";
+import {
+    isValidChecksumAddress,
+    bufferToHex,
+    toBuffer,
+    hashPersonalMessage,
+    fromRpcSig,
+    ecrecover,
+    publicToAddress,
+} from "ethereumjs-util";
 import "dotenv/config";
 
 const client = new ApolloClient({
@@ -17,12 +26,11 @@ const client = new ApolloClient({
     cache: new InMemoryCache(),
 });
 
-type User = {
+type IUser = {
     insertUser: {
         address: string;
         createdAt: Date;
-        nonce: string;
-        signature: string;
+        nonce: number;
         updatedAt: Date;
         __typename: "users";
     };
@@ -43,18 +51,17 @@ authRouter.get("/auth/:walletAddress/nonce", async (request, response) => {
     }
 
     const user = await client
-        .mutate<User, { address: string; nonce: number }>({
+        .mutate<IUser, { address: string; nonce: number }>({
             mutation: gql`
                 mutation user($address: String!, $nonce: Int!) {
                     insertUser(
                         object: { address: $address, nonce: $nonce }
                         on_conflict: {
                             constraint: users_pkey
-                            update_columns: nonce
+                            update_columns: address
                         }
                     ) {
                         updatedAt
-                        signature
                         nonce
                         createdAt
                         address
@@ -82,3 +89,126 @@ authRouter.get("/auth/:walletAddress/nonce", async (request, response) => {
     response.status(200);
     response.send(user);
 });
+
+type QUser = {
+    user: {
+        address: string;
+        createdAt: Date;
+        nonce: number;
+        updatedAt: Date;
+        __typename: "users";
+    };
+};
+
+authRouter.get(
+    "/auth/:walletAddress/signature/:signature",
+    async (request, response) => {
+        const walletAddress = request.params["walletAddress"];
+        const signature = request.params["signature"];
+
+        const isValid = isValidChecksumAddress(walletAddress);
+
+        if (!isValid) {
+            response.status(401);
+            response.send("address not valid");
+
+            return;
+        }
+
+        const user = await client
+            .query<QUser, { address: string }>({
+                query: gql`
+                    query user($address: String!) {
+                        user(address: $address) {
+                            address
+                            createdAt
+                            nonce
+                            updatedAt
+                        }
+                    }
+                `,
+                variables: {
+                    address: walletAddress,
+                },
+            })
+            .then((result) => {
+                const { data, error } = result;
+
+                if (error) {
+                    response.status(401);
+                    response.send(error.message);
+
+                    return;
+                }
+
+                return data.user;
+            });
+
+        if (user) {
+            const msg = `Nonce: ${user.nonce}`;
+            const msgHex = bufferToHex(Buffer.from(msg));
+            const msgBuffer = toBuffer(msgHex);
+            const msgHash = hashPersonalMessage(msgBuffer);
+            const signatureBuffer = toBuffer(signature);
+            const singatureParams = fromRpcSig(signatureBuffer);
+            const publicKey = ecrecover(
+                msgHash,
+                singatureParams.v,
+                singatureParams.r,
+                singatureParams.s
+            );
+            const addressBuffer = publicToAddress(publicKey);
+            const address = bufferToHex(addressBuffer);
+
+            if (address.toLowerCase() === walletAddress.toLowerCase()) {
+                await client
+                    .mutate({
+                        mutation: gql`
+                            mutation updateNonce(
+                                $address: String!
+                                $nonce: Int!
+                            ) {
+                                updateUser(
+                                    pk_columns: { address: $address }
+                                    _set: { nonce: $nonce }
+                                ) {
+                                    nonce
+                                }
+                            }
+                        `,
+                        variables: {
+                            address: walletAddress,
+                            nonce: Math.floor(Math.random() * 1000000),
+                        },
+                    })
+                    .then((result) => {
+                        const { errors } = result;
+
+                        if (errors) {
+                            response.status(401);
+                            response.send(errors[0].message);
+
+                            return;
+                        }
+                    });
+
+                const token = jwt.sign(
+                    {
+                        address: user.address,
+                    },
+                    process.env.JWT_SECRET,
+                    { expiresIn: "6h" }
+                );
+
+                response.status(200).json({
+                    token: `Bearer ${token}`,
+                    user,
+                });
+            } else {
+                response.status(401).send("Invalid credentials");
+            }
+        } else {
+            response.send("User does not exist");
+        }
+    }
+);
