1bf4b10fca1117c3b5a1575e09e416c3f94dffb1
Afonso Escudeiro <afonso_escudeiro@hotmail.com>
2022-01-31 19:46:42 -0100

    removed apollo-fetch to added apollo-client and created get user

--- routes/auth/auth.ts
+++ routes/auth/auth.ts
@@ -1,8 +1,42 @@
 import { Router } from "express";
-import { createApolloFetch } from "apollo-fetch";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client/core";
+import "dotenv/config";
+
+const client = new ApolloClient({
+    uri: process.env.HASURA_URI,
+    cache: new InMemoryCache(),
+});
 
 export const authRouter = Router();
 
-authRouter.get("/auth", (request, response) => {
-    response.send("Auth");
+authRouter.get("/auth/:walletAddress", async (request, response) => {
+    const walletAddress = request.params["walletAddress"];
+
+    const user = client
+        .query({
+            query: gql`
+                query getUser($address: String!) {
+                    user(address: $address) {
+                        address
+                        createdAt
+                        nonce
+                        signature
+                        updatedAt
+                    }
+                }
+            `,
+            variables: {
+                address: walletAddress,
+            },
+        })
+        .then((result) => {
+            const { data, error } = result;
+            if (error) {
+                return error;
+            }
+
+            return data;
+        });
+
+    response.send(user);
 });
