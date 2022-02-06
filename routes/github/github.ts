c15fc54ca947cd04cc5eca72535764acf872c29b
Afonso Escudeiro <afonso_escudeiro@hotmail.com>
2022-02-06 19:27:33 -0100

    created github app client

--- routes/github/github.ts
+++ routes/github/github.ts
@@ -1,6 +1,12 @@
 import { Router } from "express";
+import { App } from "@octokit/app";
 
 export const githubRouter = Router();
+
+const app = new App({
+    appId: process.env.GITHUB_APP_ID,
+    privateKey: process.env.GITHUB_APP_PRIVATE_KEY,
+});
 
 githubRouter.get("/github/auth", async (resquest, response) => {
     response.status(200);
